339b3551201ca1a699024814a9d78af76c44d648
VagishRa <55975163+VagishRa@users.noreply.github.com>
2020-03-23 12:57:13 +0100

    Gjortklart miniräknaruppgiften samt finputsat java

--- Miniräknare/main.js
+++ Miniräknare/main.js
@@ -3,7 +3,7 @@ function dis(val)
              document.getElementById("result").value+=val 
          } 
            
-         //function that evaluates the digit and return result 
+       
 function solve() 
          { 
              let x = document.getElementById("result").value 
@@ -11,7 +11,7 @@ function solve()
              document.getElementById("result").value = y 
          } 
            
-         //function that clear the display 
+         
 function clr() 
          { 
              document.getElementById("result").value = "" 
